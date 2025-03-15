3eec97c3f3014bb59c95a8c703f205f9e2be7472
Nivedita S <niveditasenthilkumar@gmail.com>
2025-03-15 22:12:22 +0530

    Update register.sql
    this is new

--- register.sql
+++ register.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE booknow;
 
-use booknow;23
+use booknow;
 
 CREATE table register(Ename varchar(25),Sname varchar(25), Topic varchar(25), Venue varchar(25), sdate varchar(25), stime varchar(25), etime varchar(25));
